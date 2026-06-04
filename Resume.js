76ca11cd98655e5fb0e1267a1dbc60e869a7b752
Kshitij Gawande <kshitijgawande47@gmail.com>
2026-06-04 12:55:15 +0530

    RESUME IN JS

---
+++ Resume.js
@@ -0,0 +1,58 @@
+const resume={
+    
+    PersonalDetails:[{
+        Row1: { Name: "Kshitij Gawande"},
+
+    address: {
+       District: "Pune",
+        state: "Maharashtra",
+        country:"India"
+    },
+    Contact: 
+    {
+        gmail: "kshitijgawande47@gmail.com",
+        phone: "8788453211",
+    },
+    Education: [
+        {
+            Education: "Diploma in Computer Technology",
+            College: "Venutai Chavan Polytechnic College",
+        }
+    ]
+}
+],
+         TechnicalSkills: [
+        {id: 1, skillName: "C", expertiseLevel: "Novice"},
+        {id: 2, skillName: "C", expertiseLevel: "Novice"},
+        {id: 3, skillName: "HTML", expertiseLevel: "intermediate"},   
+        {id: 4, skillName: "CSS", expertiseLevel: "intermediate"},
+         ],
+    Projects: [
+        
+        
+        {   
+            ProjectId: 1,
+            Name:"Routine Tracker",
+            Description:"A web application that helps users track their daily routines and habits. It allows users to create and manage their routines, set reminders, and visualize their progress over time.",
+            LanguagesUsed: ["Kotlin","JavaScript"]
+        },
+        {
+            ProjectId: 2,
+            Name:"Student Interaction Website",
+            Description:"A platform for students to interact with each other and with instructors. It includes features like discussion forums, assignment submission, and grade tracking.",
+            LanguagesUsed: ["Python"],
+        },
+        {
+            ProjectId: 3,
+            Name:"Dental clinic Website",
+            Description:"It was a frontend UI created using Html CSS.",
+            LanguagesUsed: ["HTML","CSS"],
+        }
+    
+    
+    ]
+    
+        
+}
+console.log(resume.PersonalDetails[0].Contact);
+console.log(resume.PersonalDetails[0].Contact.phone);
